485599c63a2dec03254d0de14d4b4798a516b08c
heysushil <sushil.chaudhary111@gmail.com>
2020-10-05 19:53:40 +0530

    first commit

---
+++ 1.introductionWithPython.py
@@ -0,0 +1,20 @@
+print('Hello python')
+
+'''
+Python work:
+
+input => process => output
+
+1. output dekhne ke liye: print()
+2. input dene ke liye: input()
+
+Python sikhne ke sath me kuch important special character ke name yaad kar lo:
+'''
+# () {} [], . : ; ' " ! - _ / \ | ~ `
+
+"""
+Commont ka use kya hai aur kaise use karte hain?
+"""
+
+# Hash ke sath hum single line commant likhte hain
+# tripal sigle ya double qoute ke sath hum multi line comment likhte hain.
---
+++ 2.pythonSyntaxAndIntentation.py
@@ -0,0 +1,12 @@
+'''
+Python intentation ka kya matlab hai?
+
+1. Iska matalb hai ki code ko likhne ka ek pattern hai aur usko follow karna jarui hai.
+'''
+
+print('Hello python intentation')
+
+if 5 < 10:
+    print('Conditon is right')
+
+print('Conditon is right')
