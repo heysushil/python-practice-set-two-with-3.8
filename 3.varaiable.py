0746cdf24e73e11b6cd2b415b626d677be418856
heysushil <sushil.chaudhary111@gmail.com>
2020-10-06 19:15:20 +0530

    variable fire

---
+++ 3.varaiable.py
@@ -0,0 +1,30 @@
+'''
+ab = 3
+b = 5
+a sum = a + b
+a_sum = a + b
+a9989 = a + b
+
+Varable works and taks: Term condtions of varaible
+
+1. variable kisi bhi value ko store karta hai
+2. varaibel unique hota hai. means small a aur capital A 2 alag varaibles hain.
+3. varaibel ke name kabhi bhi digit se start nahi hoga: 9a not acceptable
+4. variable me _ undersccore ka use kar sakte hai: _a or a_
+5. varaible me space ya other spaacial character nahi use hote hai.
+'''
+
+a = 3
+b = 5
+asum = a + b
+a_sum = a + b
+a9989 = a + b
+
+name = 'Sum of asum: '
+print('Hello python')
+print(name, asum)
+
+
+# ek sath 3 variable ko values allot karna
+a = 10
+b = 20
